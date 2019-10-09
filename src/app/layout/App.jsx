8eecdbfb849b70b5448e04ac3ed0cdaf6bdd17b9
Ruben Cancino <luxsoft.cancino@gmail.com>
2019-10-09 11:15:48 -0500

    Initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+export class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Re Events</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
